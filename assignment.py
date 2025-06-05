ef7713b6e2b9d22d234a273dbf64478d2d892a47
Charanpreet Singh <121856436+Ioncrade@users.noreply.github.com>
2025-06-05 19:22:08 +0530

    Assignment - Print the triangle pattern

---
+++ assignment.py
@@ -0,0 +1,25 @@
+def lower_triangle(n):
+    print("Lower Triangular Pattern:")
+    for i in range(1, n+1):
+        print("*" * i)
+    print()
+
+
+def upper_triangle(n):
+    print("Upper Triangular Pattern:")
+    for i in range(n, 0, -1):
+        print(" " * (n - i) + "*" * i)
+    print()
+
+
+def pyramid(n):
+    print("Pyramid Pattern:")
+    for i in range(1, n+1):
+        print(" " * (n - i) + "*" * (2*i - 1))
+    print()
+
+
+rows = 5
+lower_triangle(rows)
+upper_triangle(rows)
+pyramid(rows)
